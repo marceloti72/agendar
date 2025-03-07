5e3d392a9d03e9d450cabd9529c73867d9b13a0f
marceloti72 <marceloferreira.ti72@gmail.com>
2025-03-07 17:53:09 -0300

    070325/28

--- pagamentos/pagar_produto.php
+++ pagamentos/pagar_produto.php
@@ -180,7 +180,7 @@ $sobrenome = $_REQUEST["sobrenome"];
                                 .then((response) => {
                                     // receber o resultado do pagamento
                                     if (response.status == true) {
-                                        window.location.href = "<?=$url;?>pagamentos/pagar_produto.php?id="+response.id+'&id_produto=' + id_conta;
+                                        window.location.href = "<?=$url;?>pagamentos/pagar_produto.php?id="+response.id+'&id_produto=' + id_conta +'&id_conta='+<?=$id_conta;?>;
                                     }
                                     if (response.status != true) {
                                         alert(response.message);
