78f37370b53e381ea07c94874f03b7c0acc877b5
marceloti72 <marceloferreira.ti72@gmail.com>
2025-03-21 02:16:54 -0300

    210325/4

--- site.php
+++ site.php
@@ -592,19 +592,22 @@ $(document).ready(function(){
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3
+                    slidesToShow: 3,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 2,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
