4bebad899d595eb0dcff4fa354bead891d83d8f8
marceloti72 <marceloferreira.ti72@gmail.com>
2025-03-21 13:42:53 -0300

    210325/10

--- site.php
+++ site.php
@@ -597,24 +597,20 @@ $(document).ready(function(){
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
+                    slidesToShow: 3,                    
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
+                    slidesToShow: 2,                    
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true, // Garante que o carrossel continue funcionando
-                    adaptiveHeight: true // Ajusta a altura automaticamente, se necessário
+                    slidesToShow: 1                    
+                    
                 }
             }
         ]
