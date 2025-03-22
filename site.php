cb53aea5f9777bc9c17914739ec752a3497ca805
marceloti72 <marceloferreira.ti72@gmail.com>
2025-03-22 02:00:26 -0300

    220325/21

--- site.php
+++ site.php
@@ -519,15 +519,14 @@ if($depoimentos2 == 'Sim'){
     //     ]
     //   });
            
-   
     
     
-    console.log("Inicializando Slick Slider...");
+    
     var slickSlider = $('.slick-services').slick({
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 4,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
@@ -538,19 +537,19 @@ if($depoimentos2 == 'Sim'){
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 1,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 390,
+                breakpoint: 580,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
