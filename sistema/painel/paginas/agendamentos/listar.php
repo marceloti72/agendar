1a77d1bc53fcf662bc4e4d3a2ef248c208db7156
marceloti72 <marceloferreira.ti72@gmail.com>
2025-04-07 18:40:55 -0300

    070425/37

--- sistema/painel/paginas/agendamentos/listar.php
+++ sistema/painel/paginas/agendamentos/listar.php
@@ -253,9 +253,11 @@ if ($total_reg > 0) {
 		if ($valor_serv == $valor_pago) {
 			$valor_pagoF = 'Pagamento concluido!';
 			$cor = '#90EE90';
+			$finalizado = '';
 		} else {
 			$valor_pagoF = 'Sinal pago R$ ' . $valor_pagoF;
 			$cor = '';
+			$finalizado = '';
 		}
 
 		if ($valor_pago > 0) {
