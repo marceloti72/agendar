f14577b6004883179a51c97b976e318ca5493e25
marceloti72 <marceloferreira.ti72@gmail.com>
2025-06-30 10:20:11 -0300

    300625/2

--- sistema/painel/paginas/comentarios/salvar.php
+++ sistema/painel/paginas/comentarios/salvar.php
@@ -73,7 +73,31 @@ if ($api == 'Sim') {
     $mensagem .= 'Depoimento: ' . $texto . '%0A%0A';
     $mensagem .= 'Ja esta disponível em seu APP para ativar.%0A';    
 
-     require('../ajax/api-texto.php');    
+	$mensagem = str_replace("%0A", "\n", $mensagem); 
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://chatbot.menuia.com/api/create-message',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS => array(
+      'appkey' => $instancia,
+      'authkey' => $token,
+      'to' => $telefone,      
+      'message' => $mensagem,
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $response = json_decode($response, true);
+
+     //require('../ajax/api-texto.php');    
 }
 
 echo 'Salvo com Sucesso';
