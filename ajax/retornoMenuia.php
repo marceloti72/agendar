b2deb14020be31cfe0fc4c9c6ff39d38612810cb
marceloti72 <marceloferreira.ti72@gmail.com>
2025-07-10 13:12:03 -0300

    100725/3

--- ajax/retornoMenuia.php
+++ ajax/retornoMenuia.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once("../sistema/conexao.php");
+
 $dados = json_decode(file_get_contents('php://input'), true);
 
 $tipo = $dados['tipo'];
@@ -21,7 +22,7 @@ if ($tipo == "Sistema") {
     $dataHoraAtual = date('Y-m-d H:i:s');
 
     // Montando a consulta SQL com a condição desejada
-    $query = "SELECT * FROM agendamentos WHERE phone = '$numeroCliente' AND status = 'Agendado' AND CONCAT(data, ' ', hora) > '$dataHoraAtual' and id_conta = '$id_conta' ORDER BY CONCAT(data, ' ', hora) ASC LIMIT 1";
+    $query = "SELECT * FROM agendamentos WHERE phone = '$numeroCliente' AND status = 'Agendado' AND CONCAT(data, ' ', hora) > '$dataHoraAtual' ORDER BY CONCAT(data, ' ', hora) ASC LIMIT 1";
     $agendamento = $pdo->query($query);
     $resAgendamento = $agendamento->fetchAll(PDO::FETCH_ASSOC);
     $agendamento = $resAgendamento[0];
@@ -30,11 +31,12 @@ if ($tipo == "Sistema") {
     // Dados Agendamento
     $idCliente = $resAgendamento[0]['cliente'];
     $idFuncionario = $resAgendamento[0]['funcionario'];
+    $idAgendamento = $resAgendamento[0]['id'];
     $dataAgendamento = date('d/m/Y', strtotime($resAgendamento[0]['data'])); // data Formatada para padrao 00/00/0000
     $horarioAgendamento = $resAgendamento[0]['hora'];
 
     // Buscando infos cliente
-    $query = "SELECT * FROM clientes where id = '$idCliente' and id_conta = '$id_conta'";
+    $query = "SELECT * FROM clientes where id = '$idCliente' ";
     $cliente = $pdo->query($query);
     $resCliente = $cliente->fetchAll(PDO::FETCH_ASSOC);
     $cliente = $resCliente[0];
@@ -42,7 +44,7 @@ if ($tipo == "Sistema") {
 
 
     // Buscando infos funcionario
-    $query = "SELECT * FROM usuarios where id = '$idFuncionario' and id_conta = '$id_conta'";
+    $query = "SELECT * FROM usuarios where id = '$idFuncionario' ";
     $funcionario = $pdo->query($query);
     $resFuncionario = $funcionario->fetchAll(PDO::FETCH_ASSOC);
     $funcionario = $resFuncionario[0];
@@ -52,7 +54,7 @@ if ($tipo == "Sistema") {
     if ($agendamento) {
 
         if ($resposta == 1) {
-            $pdo->query("UPDATE agendamentos SET status = 'Confirmado' where phone = '$numeroCliente' and id_conta = '$id_conta'");
+            $pdo->query("UPDATE agendamentos SET status = 'Confirmado' where id = '$idAgendamento'");
             $mensagem = "✅ _Confirmado!_\n\n";
             $mensagem .= "Agendamento de *" . $nomeCliente . "*.\n\n";
             $mensagem .= "*Data:* " . $dataAgendamento . " às " . $horarioAgendamento . "\n\n";
@@ -64,18 +66,18 @@ if ($tipo == "Sistema") {
             require('api-texto.php');
 
             //Notifica o Dono da plataforma
-            $telefone = '55' . preg_replace('/[ ()-]+/', '', $whatsapp_sistema);
-            require('api-texto.php');
+            // $telefone = '55' . preg_replace('/[ ()-]+/', '', $whatsapp_sistema);
+            // require('api-texto.php');
 
             //Notifica o funcionario se ele nao for o dono
-            if ($telefoneFuncionario != $whatsapp_sistema) {
-                $telefone = '55' . preg_replace('/[ ()-]+/', '', $telefoneFuncionario);
-                require('api-texto.php');
-            }
+            // if ($telefoneFuncionario != $whatsapp_sistema) {
+            //     $telefone = '55' . preg_replace('/[ ()-]+/', '', $telefoneFuncionario);
+            //     require('api-texto.php');
+            // }
 
             return "Confirmado!";
         } else if ($resposta == 2) {
-            $pdo->query("DELETE FROM agendamentos where phone = '$numeroCliente' and id_conta = '$id_conta'");
+            $pdo->query("DELETE FROM agendamentos where id = '$idAgendamento'");
             $mensagem = "❌ _Cancelado!_\n\n";
             $mensagem .= "Agendamento de *" . $nomeCliente . "*\n\n";
             $mensagem .= "*Data:* " . $dataAgendamento . " às " . $horarioAgendamento . "\n\n";
@@ -86,14 +88,14 @@ if ($tipo == "Sistema") {
             require('api-texto.php');
 
             //Notifica o Dono da plataforma
-            $telefone = '55' . preg_replace('/[ ()-]+/', '', $whatsapp_sistema);
-            require('api-texto.php');
+            // $telefone = '55' . preg_replace('/[ ()-]+/', '', $whatsapp_sistema);
+            // require('api-texto.php');
 
             //Notifica o funcionario se ele nao for o dono
-            if ($telefoneFuncionario != $whatsapp_sistema) {
-                $telefone = '55' . preg_replace('/[ ()-]+/', '', $telefoneFuncionario);
-                require('api-texto.php');
-            }
+            // if ($telefoneFuncionario != $whatsapp_sistema) {
+            //     $telefone = '55' . preg_replace('/[ ()-]+/', '', $telefoneFuncionario);
+            //     require('api-texto.php');
+            // }
             return "Cancelado!";
         } else {
             $mensagem = "Ops! Nenhuma opção valida digite ✔ *1* Para confirmar ou ❌ *2* para cancelar ";
