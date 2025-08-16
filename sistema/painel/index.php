d7ef8a9797a840e3bf1eb8a4d9699a40bca341d7
marceloti72 <marceloferreira.ti72@gmail.com>
2025-08-16 06:32:09 -0300

    160825/5

--- sistema/painel/index.php
+++ sistema/painel/index.php
@@ -156,6 +156,7 @@ $plano = $res3['plano'];
 	background-image: linear-gradient(to right, #6a85b6, #bac8e0);
 	background-image: linear-gradient(to right, #ff9966, #ff5e62); */
 	background-image: linear-gradient(to right, #434371, #9669a0);
+	color: white;
 
 	text-transform: uppercase;
   }
