bc0be030b90446ed6f6d50b3a876f2a5b485cdf4
marceloti72 <marceloferreira.ti72@gmail.com>
2025-08-19 09:32:41 -0300

    190825/36

--- sistema/painel/index.php
+++ sistema/painel/index.php
@@ -875,7 +875,7 @@ $plano = $res3['plano'];
 						?>
 
 						<li class="dropdown head-dpdn">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-calendar <?php echo $icon2?>" title="Agendamentos hoje"></i>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-calendar-check" title="Agendamentos hoje"></i>
 							<?php 								
 								if($total_agendamentos_hoje_usuario_pendentes != 0){							
 									?>
@@ -940,7 +940,7 @@ $plano = $res3['plano'];
 						</li>	
 
 						<li class="dropdown head-dpdn">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bell <?php echo $icon2?>" title="Encaixes hoje"></i>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bell" title="Encaixes hoje"></i>
 							<?php 								
 								if($total_encaixes_hoje != 0){							
 									?>
