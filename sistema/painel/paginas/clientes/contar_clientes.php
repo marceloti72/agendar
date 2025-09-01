28d9a17605e3c7e7f1823838b879cdf94b46eae8
marceloti72 <marceloferreira.ti72@gmail.com>
2025-08-31 21:30:08 -0300

    310825/15

--- sistema/painel/paginas/clientes/contar_clientes.php
+++ sistema/painel/paginas/clientes/contar_clientes.php
@@ -1,13 +1,12 @@
 <?php
 @session_start();
-require_once("../../../conexao.php");
+require_once("../../conexao.php");
 
 $id_conta = $_SESSION['id_conta'];
 
 try {
-    $query = $pdo->query("SELECT COUNT(*) as total FROM clientes WHERE id_conta = :id_conta");
-    $query->bindValue(':id_conta', $id_conta);
-    $query->execute();
+    $query = $pdo->prepare("SELECT COUNT(*) as total FROM clientes WHERE id_conta = ?");
+    $query->execute([$id_conta]);
     $result = $query->fetch(PDO::FETCH_ASSOC);
     echo json_encode(['total' => $result['total']]);
 } catch (PDOException $e) {
