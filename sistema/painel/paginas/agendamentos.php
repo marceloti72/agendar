7c2bcb16c993b019f1a8549a11d5829e93df1029
marceloti72 <marceloferreira.ti72@gmail.com>
2025-09-05 21:36:09 -0300

    050925/66

--- sistema/painel/paginas/agendamentos.php
+++ sistema/painel/paginas/agendamentos.php
@@ -606,7 +606,7 @@ if (@$_SESSION['nivel_usuario'] != 'administrador') {
 		// 	stylePast: true,
 		// 	disablePast: true
 		// });
-         // ===================================================================
+        // ===================================================================
         // FIX: LÓGICA PARA CONTROLAR MODAIS SEM BOOTSTRAP.JS
         // ===================================================================
         function showModal(selector) {
@@ -619,17 +619,15 @@ if (@$_SESSION['nivel_usuario'] != 'administrador') {
             });
         }
         
-        // CORREÇÃO APLICADA AQUI
+        // jQuery continua a 'ouvir' por cliques em data-toggle, mas agora o HTML impede o problema
         $('[data-toggle="modal"]').on('click', function(e) {
-            e.preventDefault();
-            e.stopPropagation(); // Impede que o clique feche o menu lateral
+            e.preventDefault(); 
             var target = $(this).data('target');
             showModal(target);
         });
 
         $(document).on('click', '[data-dismiss="modal"]', function(e) {
             e.preventDefault();
-            e.stopPropagation(); // Também é uma boa prática aqui
             var modal = $(this).closest('.modal');
             hideModal(modal);
         });
