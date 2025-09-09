d090f81dd63bb9cef25c3bfaa9aec8031309e744
marceloti72 <marceloferreira.ti72@gmail.com>
2025-09-09 16:51:43 -0300

    090925/16

--- sistema/painel/paginas/clientes/listar.php
+++ sistema/painel/paginas/clientes/listar.php
@@ -208,7 +208,7 @@ for($i=0; $i < $total_reg; $i++){
 echo <<<HTML
 <tr class="">
 <td>
-  <img src="img/clientes/{{$foto} ? {$foto} : 'sem-foto.jpg'}" width="50" height="50" class="hovv">
+  <img src="img/clientes/{$foto} ? {$foto} : 'sem-foto.jpg'" width="50" height="50" class="hovv">
   {$nome}
 </td>
 <td class="esc">{$telefone}</td>
