f58a805bfb84caf4e9b18a559d3ad0c2a2906d41
marceloti72 <marceloferreira.ti72@gmail.com>
2025-09-11 14:42:04 -0300

    110925/23

--- ajax/api-texto-ass.php
+++ ajax/api-texto-ass.php
@@ -13,7 +13,7 @@
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => 'POST',
       CURLOPT_POSTFIELDS => array(
-      'appkey' => $instancia,
+      'appkey' => 'MARONI',
       'authkey' => $token,
       'to' => $telefone_envio,
       'licence' => 'skysee',
